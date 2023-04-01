7293cdece1a45d4eabf2cd67005c7025d68a4e02
rzatalibov555 <sethubpro@gmail.com>
2023-04-01 15:11:42 +0400

    multilanguage bug fixed

--- application/controllers/LanguageSwitcher.php
+++ application/controllers/LanguageSwitcher.php
@@ -2,15 +2,31 @@
 class LanguageSwitcher extends CI_Controller
 {
     public function __construct() {
-        parent::__construct();     
+        parent::__construct();   
+          
     }
  
     function switchLang($language = "") {
         
-        $language = ($language != "") ? $language : "english";
-        $this->session->set_userdata('site_lang', $language);
-        
+        $language = ($language != "") ? $language : "az";
+       
+        $folder_langs = [
+            'az',
+            'en',
+            'ru',
+        ];
+
+
+        if( in_array( $language ,$folder_langs ) )
+        {
+            $this->session->set_userdata('site_lang', $language);   
+        }else{
+            $this->session->set_userdata('site_lang', 'az');
+        }
         redirect($_SERVER['HTTP_REFERER']);
+        
+        
+        
         
     }
 }
